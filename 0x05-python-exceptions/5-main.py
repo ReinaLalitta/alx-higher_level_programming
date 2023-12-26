7a5b3447a770bac1aaac7c03157e1cdfc9d3201b
ReinaLalitta <lalittaruby@gmail.com>
2023-12-26 22:54:42 +0000

    Write a function that raises a type exception.

--- 0x05-python-exceptions/5-main.py
+++ 0x05-python-exceptions/5-main.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
+raise_exception = __import__('5-raise_exception').raise_exception
 
-def raise_exception():
-    """Raise a TypeError exception."""
-    raise TypeError
+try:
+    raise_exception()
+except TypeError as te:
+    print("Exception raised")
