72e774a04473eab52ce4b34e910d96facf47572f
ReinaLalitta <lalittaruby@gmail.com>
2024-01-09 17:51:35 +0000

    Write an empty class Rectangle that defines a rectangle:

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+'''
+    A module that define a Rectangle class.
+'''
+
+
+class Rectangle:
+    '''
+        A Recatangle class that define a rectangle.
+    '''
+
+    def __init__(self) -> None:
+        '''
+            Initialized a new instance.
+        '''
+    pass
