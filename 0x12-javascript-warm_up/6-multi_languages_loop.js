577e9a9597af8481e6cfe6be53972261432c1976
ReinaLalitta <lalittaruby@gmail.com>
2024-04-02 14:24:13 -0700

    update

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,9 +1,5 @@
 #!/usr/bin/node
-const myArray = [
-  'C is fun',
-  'Python is cool',
-  'Javascript is amazing'
-];
-for (let i = 0; i < myArray.length; i++) {
-  console.log(myArray[i]);
+const array = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+for (let i = 0; i < 3; i++) {
+  console.log(array[i]);
 }
