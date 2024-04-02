a5211037571acb0615e6befcbf7e20093f26a002
ReinaLalitta <lalittaruby@gmail.com>
2024-04-02 15:38:26 -0700

    Update

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,19 +1,8 @@
 #!/usr/bin/node
-const SquareP = require('./5-square');
-
-class Square extends SquareP {
+module.exports = class Square extends require('./5-square') {
   charPrint (c) {
-    if (c === undefined) {
-      c = 'X';
-    }
-    for (let i = 0; i < this.height; i++) {
-      let s = '';
-      for (let j = 0; j < this.width; j++) {
-        s += c;
-      }
-      console.log(s);
+    if (c === undefined) { this.print(); } else {
+      for (let i = 0; i < this.height; i++) { console.log(c.repeat(this.width)); }
     }
   }
-}
-
-module.exports = Square;
+};
