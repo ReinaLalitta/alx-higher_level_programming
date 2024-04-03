1a83c8b4720ebc22e2edff0c7dc58e4cf8b37b8a
ReinaLalitta <lalittaruby@gmail.com>
2024-04-03 04:41:13 -0700

    Write a script that lists all cities from the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+takes in an argument and displays all values in the states table
+of hbtn_0e_0_usa where name matches the argument
+"""
+import sys
+import MySQLdb
+
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(port=3306,
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
+                         db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name='{}'".format(sys.argv[4],))
+    states = cur.fetchall()
+
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
